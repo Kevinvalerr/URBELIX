d35e0c924643f4bdbd2cf941be20c1216f6566d5
Kevinvalerr <kevinvalerohuertas@gmail.com>
2026-02-28 16:52:54 -0500

    feat:agregado POSTpara guardar usuario desde vista

--- src/main/java/com/nexur/nexur/controller/VistaController.java
+++ src/main/java/com/nexur/nexur/controller/VistaController.java
@@ -4,6 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.nexur.nexur.service.UsuarioService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.nexur.nexur.model.Usuario;
+
+
+
 
 @Controller
 public class VistaController {
@@ -19,4 +25,22 @@ public class VistaController {
         model.addAttribute("usuarios", usuarioService.listarUsuarios());
         return "usuarios";
     }
+     @PostMapping("/guardar-usuario")
+ public String guardarUsuario(
+    @RequestParam String nombre,
+    @RequestParam String email,
+    @RequestParam String password){
+
+
+        Usuario usuario = new Usuario();
+        usuario.setNombre(nombre);
+        usuario.setEmail(email);
+        usuario.setPassword(password);
+
+        usuarioService.guardarUsuario(usuario);
+
+        return "redirect:/usuarios-vista";
+    }
 }
+
+
